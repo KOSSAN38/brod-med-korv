c3a13cac872627bcfcb3406f96e57537ac573c9b
Isak Edlund <isak.edlund@learnet.se>
2019-09-06 11:21:53 +0200

    first commit

---
+++ Skoldags.java
@@ -0,0 +1,11 @@
+Program: Skoldags
+    START( ögon):
+	GÅ(garderob):
+	KLÄ(på dig):
+	GÅ(skola):
+	START(jobba):
+	START(stress och trott):
+	STOP(jobba):
+	GÅ(hem):
+	GÅ(säng):
+	SOV
---
+++ Test1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+     public class Test1 {
+	 public static void main(String[] args) {
+	    JOptionPane.showMessageDialog(null, "https://www.youtube.com/watch?v=8ci2hj7CSHI");
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null, " Det har ar inte google");
+	 }
+	 }
